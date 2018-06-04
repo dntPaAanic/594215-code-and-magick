b7a11142743c747af27578282011174a31ce91d8
Vitaly Mes <volkondr@ya.ru>
2018-06-04 12:26:09 +0300

    Добавляет статистику

--- js/stat.js
+++ js/stat.js
@@ -22,13 +22,11 @@ var renderCloud = function (ctx, x, y, color) {
 
 var getMaxElement = function (arr) {
   var maxElement = arr[0];
-
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] > maxElement) {
       maxElement = arr[i];
     }
   }
-
   return maxElement;
 };
 
